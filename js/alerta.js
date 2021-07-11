a4f10de5501fa1643579a2d66d2d121b5212de25
Agustin Franco <francoagustinfacundo@gmail.com>
2021-07-11 19:55:29 -0300

    initial commit

---
+++ js/alerta.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('.modal').modal({ dismissible: false });
+
+    $('#modal1').modal('open');
+});
---
+++ js/chart.js
@@ -0,0 +1,31 @@
+
+
+var labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+
+var data = {
+    labels: labels,
+    datasets: [{
+        label: 'Bienestar',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 25, 30, 50, 60, 70, 80],
+    }]
+};
+
+var config = {
+    type: 'line',
+    data,
+    options: {}
+};
+
+var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
